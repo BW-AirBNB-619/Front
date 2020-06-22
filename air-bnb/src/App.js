e055fdcfc0ae58145a3f45243e2588867116f974
troyschennum <tj.schennum@gmail.com>
2020-06-22 15:25:09 -0500

    deploy test

--- air-bnb/src/App.js
+++ air-bnb/src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Message from './component/MessageComponent'
-import Form from './component/FormComponent'
+import {Hello} from './component/hello'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello World
-      </header>
+     <Hello />
     </div>
   );
 }

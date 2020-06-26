f4fdc9d5ad67f366c1749c2f0cc302f7866b7184
Alex Kemper <alexkemper@Alexs-MacBook-Pro.local>
2020-06-25 20:01:11 -0400

    updated marketing link

--- air-bnb/src/App.js
+++ air-bnb/src/App.js
@@ -1,21 +1,17 @@
-import Dashboard from './component/Dashboard'
-import PrivateRoute from "./component/PrivateRoute"
+import Dashboard from "./component/Dashboard";
+import PrivateRoute from "./component/PrivateRoute";
 import React from "react";
-import { Switch, Route, NavLink} from "react-router-dom";
+import { Switch, Route, NavLink } from "react-router-dom";
 import "./App.css";
 import SignInForm from "./component/SignInForm";
-import SignUpForm from './component/SignUpForm';
+import SignUpForm from "./component/SignUpForm";
 import HeaderNav from "./component/Header";
 import FooterNav from "./component/Footer";
 import AppContainer from "./component/Styles/AppContainerStyles";
 import BodyContainer from "./component/Styles/BodyContainerStyles";
 
-
-
 function App() {
-
   return (
-   
     <AppContainer>
       <HeaderNav />
       <BodyContainer>
@@ -27,9 +23,10 @@ function App() {
             path="/marketing"
             component={() => {
               window.location.href =
-                "https://bw-airbnb-619.netlify.app/team/team.html";
+                "https://https://bw-airbnb-619.netlify.app/";
               return null;
             }}
+            target="_blank"
           />
           <Route path="/sign-in">
             <SignInForm />
